dd8e09c93e607ebc2fdddbe7593f745f68518804
ma.tyurin@outlook.com <ma.tyurin@outlook.com>
2021-10-09 22:32:13 +0300

    Added script sql for import from csv (dbf)

--- ModulesPyOnly/service_func.py
+++ ModulesPyOnly/service_func.py
@@ -30,7 +30,9 @@ def LemmatizeAddrBase():
                                                               full_addr, \
                                                               full_addr_lemm, \
                                                               full_addr_2, \
-                                                              full_addr_2_lemm \
+                                                              full_addr_2_lemm, \
+                                                              full_addr_3, \
+                                                              full_addr_3_lemm \
                                                             FROM  \
                                                               git010_dict.full_fias_addr \
                                                             WHERE \
@@ -43,14 +45,17 @@ def LemmatizeAddrBase():
         
         _sentences = []
         _sentences_2 = []
+        _sentences_3 = []
         for i in non_lemmatized_recs:
             _sentences.append(i['full_addr'] if i['full_addr'] != '' else 'empty')
             _sentences_2.append(i['full_addr_2'] if i['full_addr_2'] != '' else 'empty')
+            _sentences_3.append(i['full_addr_3'] if i['full_addr_3'] != '' else 'empty')
 
         lemma_result = lemmatizer.lemmatize(_sentences)
         lemma_result_2 = lemmatizer.lemmatize(_sentences_2)
+        lemma_result_3 = lemmatizer.lemmatize(_sentences_3)
 
-        for result in zip(non_lemmatized_recs, lemma_result, _sentences, lemma_result_2, _sentences_2):
+        for result in zip(non_lemmatized_recs, lemma_result, _sentences, lemma_result_2, _sentences_2, lemma_result_3, _sentences_3):
             lemm_list = []
             for i in result[1]:
                 if i['lemma'] == 'empty':
@@ -71,13 +76,24 @@ def LemmatizeAddrBase():
                     lemm_list.append(i['lemma'])
             lemm_2 = ' '.join(lemm_list)
 
+            lemm_list = []
+            for i in result[5]:
+                if i['lemma'] == 'empty':
+                    pass
+                elif i['word'] in socr:
+                    lemm_list.append(i['word'])
+                else:
+                    lemm_list.append(i['lemma'])
+            lemm_3 = ' '.join(lemm_list)
 
-            print(f"id: {result[0]['id']} sent: {result[2]}   lemma: {lemm}   sent: {result[4]}  lemma_2:{lemm_2}")
+
+            print(f"id: {result[0]['id']} sent1: {result[2]}   lemma_1: {lemm}   sent2: {result[4]}  lemma_2:{lemm_2}  sent3: {result[6]}  lemma_3:{lemm_3}")
             cass_db.custom_simple_request(f"UPDATE \
                                               git010_dict.full_fias_addr \
                                             SET \
                                               full_addr_lemm = '{lemm}', \
-                                              full_addr_2_lemm = '{lemm_2}' \
+                                              full_addr_2_lemm = '{lemm_2}', \
+                                              full_addr_3_lemm = '{lemm_3}' \
                                             WHERE \
                                               id = {result[0]['id']}\
                                             ;")
