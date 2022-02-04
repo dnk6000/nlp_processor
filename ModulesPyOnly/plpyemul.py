49b3f9875d4e06c3d214b8f137abc74b4406217a
ma.tyurin@outlook.com <ma.tyurin@outlook.com>
2022-02-04 06:47:39 +0300

    Small improvements. Improving the stability of the db-connection

--- ModulesPyOnly/plpyemul.py
+++ ModulesPyOnly/plpyemul.py
@@ -85,7 +85,7 @@ class PlPy(object):
                 if not self._pauser.sleep():
                     raise expt
                 else:
-                    print(f'{date.date.date_now_str()} Ошибка чтения/записи в БД !!! Попытка {str(attempt)}')
+                    print(f'{date.date_now_str()} Ошибка чтения/записи в БД !!! Попытка {str(attempt)}')
                     if type(expt) == psycopg2.OperationalError and isinstance(expt.pgerror, str) and 'server closed the connection' in expt.pgerror \
                       or type(expt) == psycopg2.OperationalError and len(expt.args) >= 1 and isinstance(expt.args[0], str) and 'could not receive data from server' in expt.args[0]:
                         print('  Trying to reconnect...')
